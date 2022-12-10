a68ffc7e17606a71f68755f781bcccc4a4e45b61
Kate <ketarnaut@mail.ru>
2022-12-10 17:25:20 +0300

    Сдаю дз2

---
+++ Desktop/gbc/Practic2/Ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿// сгенерировать случайное число от 10 до 99 и выбрать бОльшую цифру из 2-ух
+
+int N = new Random().Next(10, 99);
+int firstDigit = N / 10;
+int secondDigit = N % 10;
+
+Console.WriteLine(N);
+
+Console.Write("Больше число: ");
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else 
+{
+    Console.WriteLine(secondDigit);
+}
+
---
+++ Desktop/gbc/Practic2/Ex2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Программа принимает на вход 2 часла и сообщает, является ли 2-ое число кратным первому
+
+Console.WriteLine("Введите число 1");
+int firstDigit = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int secondDigit = Convert.ToInt32(Console.ReadLine());
+if (firstDigit % secondDigit == 0 )
+{
+    Console.WriteLine("Число 2 кратно числу 1");
+}
+
+else 
+{
+    Console.WriteLine("Число 2 не кратно числу 1");
+}
